883839a379d0dda28f742e1a3bf533a9f83d63ae
Nathan Hans <nathaniel.hans@gmail.com>
2024-02-19 21:02:46 -0500

    temp fix for about in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,6 +13,11 @@ export const Navbar = () => {
             <div className='hidden md:flex mr-5'>
                 <NavCategories/>
             </div>
+            <Link href={'/about'} className='md:hidden pr-2'>
+                <div className="text-lg  md:flex">
+                    About
+                </div>
+            </Link>
         </div>
     );
 };
--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,8 +1,8 @@
 import { Category, EventsMap, EventsResult, Production, Productions, VendorPartial } from '@/utils/models';
 
 export class APIService {
-    static baseURL = 'https://ticket-iq-production.up.railway.app';
-    // static baseURL = 'http://localhost:7000';
+    // static baseURL = 'https://ticket-iq-production.up.railway.app';
+    static baseURL = 'http://localhost:7000';
     // how long a API request for data is cached
     static CACHE_TIME_SECONDS = 100;
 
