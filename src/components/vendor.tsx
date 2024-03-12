b999f1bbfbd072be083d12b1bed61da0cf0d5b5e
Nathan Hans <nathaniel.hans@gmail.com>
2024-03-11 20:17:07 -0400

    added go to link on mobile sizes

--- src/components/vendor.tsx
+++ src/components/vendor.tsx
@@ -1,30 +1,43 @@
 import Image from 'next/image';
 import React from 'react';
-import { Production,VendorPartial } from '@/utils/models';
+import { Production, VendorPartial } from '@/utils/models';
 import { APIService } from '@/services/apiService';
+import { ExternalLink } from 'lucide-react';
+import { ArrowTopRightIcon, OpenInNewWindowIcon } from '@radix-ui/react-icons';
 
 
 export interface VendorProps {
     // productionURL: string,
     // productionDetails: Production,
-    vendor:VendorPartial,
+    vendor: VendorPartial,
     includeFees: boolean;
 }
 
 export const Vendor: React.FC<VendorProps> = ({ vendor, includeFees }) => {
-    //send log to backend server, vercel is dumb and cant log for some reason
-    const logClickThrough = async() => {
-        console.log('Attempting log of site')
-       APIService.logVendorSiteClickThrough(vendor.vendor.name);
-    };
+        //send log to backend server, vercel is dumb and cant log for some reason
+        const logClickThrough = async () => {
+            console.log('Attempting log of site');
+            APIService.logVendorSiteClickThrough(vendor.vendor.name);
+        };
 
-    return (
-        <a className=" flex font-bold text-md lg:text-lg hover:bg-[#D9DCE1] " href={vendor.url} target={'_blank'} onClick={logClickThrough }>
-            {/*<Image src={vendorIcon} alt={''}*/}
-            {/*       className=" w-[32px] h-[32px] lg:w-[64px] lg:h-[50px] rounded-md "></Image>*/}
-            <div></div>
-            <div className="pl-2 flex flex-col items-start md:w-[150px]">{vendor.vendor.displayName }{includeFees && vendor.vendor.name==='vivid_seats'?'*':'' }</div>
-            <div className="ml-auto pr-1">{`\$${includeFees ? vendor.priceWithFees: vendor.minPrice}`}</div>
-        </a>
-    );
-};
+        return (
+            <a className=" flex items-center  font-bold text-md lg:text-lg hover:bg-[#D9DCE1] " href={vendor.url}
+               target={'_blank'}
+               onClick={logClickThrough}>
+                <div></div>
+                <div
+                    className="pl-2 flex flex-col items-start md:w-[150px]">{vendor.vendor.displayName}{includeFees && vendor.vendor.name === 'vivid_seats' ? '*' : ''}</div>
+                <div className="ml-auto pr-1">{`\$${includeFees ? vendor.priceWithFees : vendor.minPrice}`}</div>
+                <div className="mr-1 md:hidden">
+                    <ArrowTopRightIcon
+                        height={20}
+                        width={20}
+                        className="pr-1 w-[30px]"
+                        href={vendor.url}
+                        onClick={logClickThrough}/>
+                </div>
+
+            </a>
+        );
+    }
+;
