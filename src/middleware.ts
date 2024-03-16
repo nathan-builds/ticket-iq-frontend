078fac2946f6e8ff4a5eb539ee0603e1c3255663
Nathan Hans <nathaniel.hans@gmail.com>
2024-03-16 13:52:45 -0400

    test geo2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,26 @@
 // middleware.ts
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 // import { geolocation } from '@vercel/edge';
 
 export default async function myMiddleware(request: NextRequest) {
   // Your Middleware logic heres
-    const { nextUrl: url, geo } = request;
-    const country = geo?.country || 'US'
-    console.log(request)
-    // const { city } = geolocation(request);
-    console.log(`Middleware got country details of ${geo?.city}, ${geo?.country}, ${geo?.latitude}, ${geo?.longitude} `);
-  return NextResponse.next(); // Pass control to the next Middleware or route handler
+  const { nextUrl: url, geo } = request;
+  const country = geo?.country || "US";
+  console.log(request);
+  // const { city } = geolocation(request);
+  console.log(
+    `Middleware got country details of ${geo?.city}, ${geo?.country}, ${geo?.latitude}, ${geo?.longitude} `
+  );
+    url.searchParams.set("country", country);
+    url.searchParams.set("city", geo?.city || 'unk');
+    url.searchParams.set("lat", geo?.latitude || 'unk');
+    url.searchParams.set("lat", geo?.longitude||'unk');
+
+
+  return NextResponse.rewrite(url)// Pass control to the next Middleware or route handler
 }
 
-
 export const config = {
-    matcher: '/results',
-  }
+  matcher: "/results",
+};
