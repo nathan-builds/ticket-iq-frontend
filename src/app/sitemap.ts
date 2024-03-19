a92a4709215a745958c7e7a7246887d2c7f29a72
Nathan Hans <nathaniel.hans@gmail.com>
2024-03-19 17:50:19 -0400

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,22 +1,38 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from 'next';
 import { APIService } from '../services/apiService';
 
 
-
-export const getSiteMapDetails=async()=>{
-    const slugs = await APIService.getSiteMapDetails();;
+export const getSiteMapDetails = async () => {
+    const slugs = await APIService.getSiteMapDetails();
+    ;
     return slugs;
 
-}
-
+};
 
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const slugs = await getSiteMapDetails();
-    return slugs.map(slug=>({
-        url:`https://mytickethero.com/results/${slug}`,
+    const staticURLS = [{
+        url: `https://mytickethero.com`,
         lastModified: new Date(),
-        changeFrequency:'daily',
-        priority:1
-    }))
+        changeFrequency: 'daily',
+        priority: 1
+    }, {
+        url: `https://mytickethero.com/about`,
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.3
+    }
+    ];
+    const allURLS = [...staticURLS];
+
+    slugs.forEach(slug => {
+        allURLS.push({
+            url: `https://mytickethero.com/results/${slug}`,
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 0.8
+        });
+    });
+    return allURLS as MetadataRoute.Sitemap;
 }
